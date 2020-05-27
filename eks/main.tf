94d48ba229a7b7d9709fcbfea57ab09321457713
Erokos <rkosanovic14@gmail.com>
2020-05-27 14:23:36 +0200

    Use terraform 0.12 EKS module
    Signed-off-by: Erokos <rkosanovic14@gmail.com>

--- eks/main.tf
+++ eks/main.tf
@@ -1,31 +1,31 @@
 provider "aws" {
-  region = "${var.region_name}"
+  region = var.region_name
 }
 
 module "eks_vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-  version              = "v1.67.0"
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "v2.32.0"
 
-  name                 = "eks-staging-vpc"
-  cidr                 = "10.0.0.0/16"
+  name = "eks-staging-vpc"
+  cidr = "10.0.0.0/16"
 
-  azs                  = ["eu-central-1a", "eu-central-1b"]
-  private_subnets      = ["10.0.0.0/18", "10.0.64.0/18"]
-  public_subnets       = ["10.0.128.0/18", "10.0.192.0/18"]
+  azs             = ["eu-central-1a", "eu-central-1b"]
+  private_subnets = ["10.0.0.0/18", "10.0.64.0/18"]
+  public_subnets  = ["10.0.128.0/18", "10.0.192.0/18"]
 
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
-  
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
   #  reuse_nat_ips        = "${var.eks-reuse-eip}"
-  enable_vpn_gateway   = false
+  enable_vpn_gateway = false
 
   #  external_nat_ip_ids  = ["${var.eks-nat-fixed-eip}"]
   enable_dns_hostnames = true
   enable_dns_support   = true
 
   tags = {
-    Terraform          = "true"
-    Environment        = "${terraform.workspace}"
+    Terraform   = "true"
+    Environment = terraform.workspace
     # Required for EKS and Kubernetes to discover and manage networking resources
     "kubernetes.io/cluster/${var.eks_cluster_name}" = "shared"
   }
@@ -36,7 +36,7 @@ resource "aws_security_group_rule" "allow_http" {
   from_port         = 80
   to_port           = 80
   protocol          = "TCP"
-  security_group_id = "${module.eks_vpc.default_security_group_id}"
+  security_group_id = module.eks_vpc.default_security_group_id
   cidr_blocks       = ["0.0.0.0/0"]
 }
 
@@ -45,59 +45,57 @@ resource "aws_security_group_rule" "eks_vpc_allow_https" {
   from_port         = 443
   to_port           = 443
   protocol          = "TCP"
-  security_group_id = "${module.eks_vpc.default_security_group_id}"
+  security_group_id = module.eks_vpc.default_security_group_id
   cidr_blocks       = ["0.0.0.0/0"]
 }
 
 module "eks" {
-  source                             = "github.com/Erokos/terraform_modules//eks?ref=9a35fc8"
+  source                             = "github.com/Erokos/terraform_modules//eks?ref=cec5da7"
+  #source                             = "../../../repos_work/terraform-modules/eks?ref=master"
   eks_cluster_name                   = "gdna-test-cluster"
-  region_name                        = "${var.region_name}"
-  source_security_group_id           = "${module.eks_vpc.default_security_group_id}"
-  vpc_id                             = "${module.eks_vpc.vpc_id}"
-  vpc_zone_identifier                = "${module.eks_vpc.private_subnets}"
-  worker_launch_template_mixed_count = 2
-  cluster_kubernetes_version         = "1.14"
-  bastion_vpc_zone_identifier        = "${module.eks_vpc.public_subnets}"
-  eks_worker_subnets                 = "${module.eks_vpc.private_subnets}"
-  key_name                           = "${var.key_name}"
-  key_value                          = "${var.key_value}"
-  aws_access_key                     = "fill_in"
-  aws_secret_access_key              = "fill_in"
+  region_name                        = var.region_name
+  source_security_group_id           = module.eks_vpc.default_security_group_id
+  vpc_id                             = module.eks_vpc.vpc_id
+  eks_worker_subnets                 = module.eks_vpc.private_subnets
+  worker_launch_config_count         = 2
+  cluster_kubernetes_version         = "1.13"
+  eks_ami_version                    = "1.13"
+  bastion_vpc_zone_identifier        = module.eks_vpc.public_subnets
+  enable_bastion                     = true
+  bastion_name                       = "gdna-ultimate-bastion"
+  key_name                           = var.key_name
+  key_value                          = var.key_value
+  pvt_key                            = var.pvt_key
+  kubectl_eks_link                   = "https://amazon-eks.s3-us-west-2.amazonaws.com/1.14.6/2019-08-22/bin/linux/amd64/kubectl"
+  cni_link                           = "https://raw.githubusercontent.com/aws/amazon-vpc-cni-k8s/release-1.5/config/v1.5/aws-k8s-cni.yaml"
+  aws_access_key                     = var.aws_access_key
+  aws_secret_access_key              = var.aws_secret_access_key
 
-  worker_launch_template_lst = [
+  worker_launch_config_lst = [
     {
       name                       = "general-purpose"
-      instance_type_pool1        = "t2.micro"
-      instance_type_pool2        = "t3.micro"
-      instance_type_pool3        = "t3.nano"
-      spot_instance_pools        = 3
+      instance_type              = "t2.micro"
+      spot_max_price             = "0.214"
       asg_max_size               = 3
       asg_desired_capacity       = 2
       kubelet_extra_args         = "lifecycle=spot,worker-type=general-purpose"
-      on_demand_base_capacity    = 0
-      instance_shutdown_behavior = "terminate"
       enable_monitoring          = false
-      key_name                   = "${var.key_name}"
-      key_value                  = "${var.key_value}"
+      key_name                   = var.key_name
+      key_value                  = var.key_value
       ebs_optimized              = false
     },
-
     {
       name                       = "compute-optimized"
-      instance_type_pool1        = "c5.large"
-      instance_type_pool2        = ""
-      instance_type_pool3        = ""
-      spot_instance_pools        = 1
+      instance_type              = "t3.large"
+      spot_max_price             = "0.192"
       asg_max_size               = 3
       asg_desired_capacity       = 2
       kubelet_extra_args         = "lifecycle=spot,worker-type=compute-optimized"
-      on_demand_base_capacity    = 0
-      instance_shutdown_behavior = "terminate"
       enable_monitoring          = false
-      key_name                   = "${var.key_name}"
-      key_value                  = "${var.key_value}"
+      key_name                   = var.key_name
+      key_value                  = var.key_value
       ebs_optimized              = false
-    }
+    },
   ]
 }
+
